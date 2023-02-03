c4d75b29de4cf2ba040d9e36d9f711fb959bae2c
IrfaanHameed <irfaanm857@gmail.com>
2023-02-04 00:01:45 +0530

    deploying..

--- Bmi_Project/settings.py
+++ Bmi_Project/settings.py
@@ -27,12 +27,12 @@ load_dotenv(os.path.join(BASE_DIR,".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-#print("SECRET KEY : ",SECRET_KEY)
+print("SECRET KEY : ",SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://computifybmiapp-production.up.railway.app/','127.0.0:1']
 
 
 

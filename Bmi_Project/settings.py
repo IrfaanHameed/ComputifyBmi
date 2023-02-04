75f085e5b6b1e87d3f46dce3cea391dd3ed41c50
IrfaanHameed <irfaanm857@gmail.com>
2023-02-04 12:31:59 +0530

    deploying computify-bmi-app

--- Bmi_Project/settings.py
+++ Bmi_Project/settings.py
@@ -135,10 +135,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
       os.path.join(BASE_DIR,"static"),
 ]
+
+STATICFILES_STORAGE = 'Bmi_Project.storage.S3Storage'
+
+
+
+
+#STATICFILES_STORAGE = 'Bmi_Project.storage.S3Storage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

bb83be1b684e52b24c5a94744a04d6aabe3d4e6d
IrfaanHameed <irfaanm857@gmail.com>
2023-02-04 13:04:23 +0530

    deploying bmi projec2t ...

--- Bmi_Project/settings.py
+++ Bmi_Project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = get_random_secret_key()
 #print("SECRET KEY : ",SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 

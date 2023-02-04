c19312d491c40ad05eae3c29531fdc78aaf3ec0a
IrfaanHameed <irfaanm857@gmail.com>
2023-02-04 13:01:45 +0530

    deploying bmi project ...

--- Bmi_Project/settings.py
+++ Bmi_Project/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Bmi_Project.urls'
@@ -134,13 +135,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
       os.path.join(BASE_DIR,"static"),
 ]
 
-STATICFILES_STORAGE = 'Bmi_Project.storage.S3Storage'
+
+DISABLE_COLLECTSTATIC = 0
 
 
 

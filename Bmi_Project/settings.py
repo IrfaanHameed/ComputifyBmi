7b9819abaadd3257571b1bf700f76de6f22eb601
IrfaanHameed <irfaanm857@gmail.com>
2023-02-04 13:18:55 +0530

    deploying bmi-app4

--- Bmi_Project/settings.py
+++ Bmi_Project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = get_random_secret_key()
 DEBUG = True
 
 if not DEBUG:
-          CSRF_TRUSTED_ORIGINS = ["https://computifybmi-production.up.railway.app/"]
+          CSRF_TRUSTED_ORIGINS = ["https://computifybmi-production.up.railway.app/check-bmi"]
 ALLOWED_HOSTS = ['*']
 
 

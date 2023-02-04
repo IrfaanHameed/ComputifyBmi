38eb70d1b342504ef078a9be8efb73aebcff3844
IrfaanHameed <irfaanm857@gmail.com>
2023-02-04 12:50:13 +0530

    deploying computify-bmi-app4

--- Bmi_Project/settings.py
+++ Bmi_Project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['computifybmi-production.up.railway.app','www.computifybmi.com']
+ALLOWED_HOSTS = ['*']
 
 
 

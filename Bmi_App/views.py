689255f858c23d60e7698477233f18e0eb4bb953
IrfaanHameed <irfaanm857@gmail.com>
2023-02-07 15:39:49 +0530

    computify bmi is ready 123....

--- Bmi_App/views.py
+++ Bmi_App/views.py
@@ -31,38 +31,36 @@ def result(request):
       moderate_risk_count = 0
       high_risk_count = 0
       low_risk_count=0
-    
-    
       total = customers.count()
-
-      bmi = customer.get_bmi
-      customer_id = customer
-      height = customer.height
-      weight = customer.weight
-      if bmi < 18.5:
-             message = "Under Weight"
-             underweight_count+=1
-             color = "light green "
-      elif bmi >= 18.5 and bmi <= 24.9:
-               message = "Normal  Weight"
-               NormalWeight_count+=1
-               color = "green"
-      elif bmi >= 25 and bmi <= 29.9:
-               message = "Pre-Obesity"
-               pre_obesity_count+=1
-               color = "orange"
-      elif bmi >= 30 and bmi <= 34.9:
-               message = "Low Risk Obesity"
-               low_risk_count+=1
-               color = "dark orange"
-      elif bmi >= 35 and bmi <= 39.9:
-               message = "Moderate Risk Obesity"
-               moderate_risk_count+=1
-               color = "red"
-      elif bmi >= 40:
-               message = "High Risk Obesity"
-               high_risk_count+=1
-               color = "dark red" 
+      for customer in customers:
+            bmi = customer.get_bmi
+            customer_id = customer
+            height = customer.height
+            weight = customer.weight
+            if bmi < 18.5:
+                  message = "Under Weight"
+                  underweight_count+=1
+                  color = "light green "
+            elif bmi >= 18.5 and bmi <= 24.9:
+                  message = "Normal  Weight"
+                  NormalWeight_count+=1
+                  color = "green"
+            elif bmi >= 25 and bmi <= 29.9:
+                  message = "Pre-Obesity"
+                  pre_obesity_count+=1
+                  color = "orange"
+            elif bmi >= 30 and bmi <= 34.9:
+                  message = "Low Risk Obesity"
+                  low_risk_count+=1
+                  color = "dark orange"
+            elif bmi >= 35 and bmi <= 39.9:
+                  message = "Moderate Risk Obesity"
+                  moderate_risk_count+=1
+                  color = "red"
+            elif bmi >= 40:
+                  message = "High Risk Obesity"
+                  high_risk_count+=1
+                  color = "dark red" 
       frontend_stuff = {"bmi":bmi,"message":message,"total":total,"highobesity":high_risk_count,
       "lowobesity":low_risk_count,"moderateobesity":moderate_risk_count,"normal":NormalWeight_count,
       "underweight":underweight_count,"preobesity":pre_obesity_count,}
